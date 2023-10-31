b1013a2a271704a7bdb725564647bb038d13d922
samaralnasib <148685912+samaralnasib@users.noreply.github.com>
2023-10-31 11:06:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the number: ");
+        int num = input.nextInt();
+
+        if (num>0){
+            System.out.println("Odd :" + num);
+        } else if (num<0) {
+            System.out.println("Even :" + num);
+
+        } else System.out.println("Zero");
+
+
+
+
+
+
+
+        /*
+        int[] num = {10, -21, 30, 31, -25};
+        for (int n : num) {
+            if (n > 0) {
+                System.out.println("Even: " + n);
+            } else if (n < 0) {
+                System.out.println("Odd: " + n);
+
+            } else System.out.println("Zero");
+
+
+         */
+
+
+          String [] word = {"Tuwaiq","Bootcamp","Student","JAVA"};
+          for (String w: word){
+              if (w);
+              System.out.println(word[w]);
+          }
+        }
+
+
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = input.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < 0; i++) {
+            System.out.println("Enter the number");
+            int in = input.nextInt();
+
+        }
+    }
+}
+
